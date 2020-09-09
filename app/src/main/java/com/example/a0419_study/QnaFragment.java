f51de748d80cc39281dc0671e018d081af3e4d59
cattish95 <wlsdhks5495@naver.com>
2020-09-09 12:30:23 +0900

    0909JW) QnA fragment 완성 (메일보내는 기능)

--- app/src/main/java/com/example/a0419_study/QnaFragment.java
+++ app/src/main/java/com/example/a0419_study/QnaFragment.java
@@ -1,21 +1,35 @@
 package com.example.a0419_study;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 public class QnaFragment extends Fragment {
-
-
-
+    Button mailBtn;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-
         View root = inflater.inflate(R.layout.fragment_qna, container, false);
+
+        mailBtn = root.findViewById(R.id.btn_qna);
+        mailBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent email = new Intent(Intent.ACTION_SEND);
+                email.setType("plain/text");
+                String[] address = {"wlsdhks5495@naver.com"};
+                email.putExtra(Intent.EXTRA_EMAIL, address);
+                email.putExtra(Intent.EXTRA_SUBJECT, "제목");
+                email.putExtra(Intent.EXTRA_TEXT, "문의해주셔서 감사합니다 :)");
+                startActivity(email);
+            }
+        });
 
         return root;
     }
